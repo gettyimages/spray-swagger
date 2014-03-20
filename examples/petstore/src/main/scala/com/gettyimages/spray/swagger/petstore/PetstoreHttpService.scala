d19f0606c294ef6727875dd8f6645cdd14f50495
Edwin Fuquen <efuquen@gmail.com>
2014-03-20 01:29:49 -0400

    added CORS to get wordnik to load spec

--- examples/petstore/src/main/scala/com/gettyimages/spray/swagger/petstore/PetstoreHttpService.scala
+++ examples/petstore/src/main/scala/com/gettyimages/spray/swagger/petstore/PetstoreHttpService.scala
@@ -6,7 +6,8 @@ import akka.actor.Actor
 
 import org.json4s.DefaultFormats
 
-import spray.routing.HttpService
+import spray.routing._
+import spray.http.HttpHeaders.RawHeader
 import spray.httpx.Json4sSupport
 
 import com.wordnik.swagger.annotations.Api
@@ -16,17 +17,36 @@ import com.wordnik.swagger.annotations.ApiModelProperty
 import com.wordnik.swagger.annotations.ApiImplicitParams
 import com.wordnik.swagger.annotations.ApiImplicitParam
 
-class PetstoreServiceActor extends Actor with PetstoreHttpService {
+import com.gettyimages.spray.swagger.SwaggerHttpService
 
-  override def json4sFormats = DefaultFormats
+import scala.reflect.runtime.universe.typeOf
+
+class PetstoreServiceActor extends Actor with HttpService {
 
   override def actorRefFactory = context
   
-  override def receive = runRoute(routes)
+  override def receive = runRoute(
+    (new PetstoreHttpService{
+      override def actorRefFactory = context
+    }).routes ~
+    //TODO: Bake in CORS support
+    respondWithHeader(RawHeader("Access-Control-Allow-Origin", "http://swagger.wordnik.com")) { (new SwaggerHttpService {
+      def actorRefFactory = context
+      def apiTypes = Seq(typeOf[PetstoreHttpService])
+      def modelTypes = Seq(typeOf[Pet])
+      def apiVersion = "1.0"
+      def swaggerVersion = "1.2"
+      def baseUrl = "http://localhost:8080"
+      def specPath = "api"
+      def resourcePath = "api-docs" 
+    }).routes }
+  )
 }
 
 @Api(value = "/pet", description = "Operations about pets")
 trait PetstoreHttpService extends HttpService with Json4sSupport {
+
+  override def json4sFormats = DefaultFormats
 
   @ApiOperation(httpMethod = "GET", response = classOf[Pet], value = "Returns a pet based on ID")
   @ApiImplicitParams(Array(
